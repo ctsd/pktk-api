fd7ff7af582d724eacd1145c2cc7f0135d7d6166
CTSD <clement.trestard@gmail.com>
2016-07-15 16:46:08 +0200

    Base API

---
+++ db/migrate/20160715142829_create_users.rb
@@ -0,0 +1,23 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.integer :team, null: false
+      t.string :messenger_id, null: false
+      t.string :lat, default: nil
+      t.string :long, default: nil
+      t.boolean :on, default: 0
+
+      t.timestamps null: false
+    end
+
+    create_table :messages do |t|
+      t.string :text, null: false
+      t.integer :user_id, null: false
+      t.string :lat, null: false
+      t.string :long, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
