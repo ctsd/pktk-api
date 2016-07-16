1d9e5db0daf01bdd69b67717c913fc28f4b1b52b
CTSD <clement.trestard@gmail.com>
2016-07-16 15:17:59 +0200

    Fixing migration for PG

--- db/migrate/20160715201459_change_lat_lng_type.rb
+++ db/migrate/20160715201459_change_lat_lng_type.rb
@@ -1,13 +1,15 @@
 class ChangeLatLngType < ActiveRecord::Migration
   def up
-    change_column :users, :lat, :float
-    change_column :users, :long, :float
-    rename_column :users, :long, :lng
+    remove_column :users, :lat
+    remove_column :users, :long
+    add_column :users, :lat, :float, null: false
+    add_column :users, :lng, :float, null: false
   end
 
   def down
-    change_column :users, :lat, :string
-    change_column :users, :lng, :string
-    rename_column :users, :lng, :long
+    remove_column :users, :lat
+    remove_column :users, :long
+    add_column :users, :lat, :string, null: false
+    add_column :users, :long, :string, null: false
   end
 end
